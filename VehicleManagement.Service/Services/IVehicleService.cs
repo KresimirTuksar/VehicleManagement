624ef4fcad783a2dab82755c2ceb76ce0e715d51
KRESO\Kreso <kresimir.tuksar@gmail.com>
2024-06-03 19:26:22 +0200

    I created a service with basic CRUD methods for both VehicleMake and VehicleModel models.

---
+++ VehicleManagement.Service/Services/IVehicleService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VehicleManagement.Service.Models;
+
+namespace VehicleManagement.Service.Services
+{
+    public interface IVehicleService
+    {
+        Task<List<VehicleMake>> GetVehicleMakesAsync();
+        Task<VehicleMake> GetVehicleMakeByIdAsync(Guid id);
+        Task CreateVehicleMakeAsync(VehicleMake vehicleMake);
+        Task UpdateVehicleMakeAsync(VehicleMake vehicleMake);
+        Task DeleteVehicleMakeAsync(VehicleMake vehicleMake);
+
+        Task<List<VehicleModel>> GetVehicleModelsAsync();
+        Task<VehicleModel> GetVehicleModelByIdAsync(Guid id);
+        Task CreateVehicleModelAsync(VehicleModel vehicleModel);
+        Task UpdateVehicleModelAsync(VehicleModel vehicleModel);
+        Task DeleteVehicleModelAsync(VehicleModel vehicleModel);
+    }
+}
---
+++ VehicleManagement.Service/Services/VehicleService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VehicleManagement.Service.Data;
+using VehicleManagement.Service.Models;
+
+namespace VehicleManagement.Service.Services
+{
+    public class VehicleService : IVehicleService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehicleService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<VehicleMake>> GetVehicleMakesAsync()
+        {
+            return await _context.VehicleMakes.ToListAsync();
+        }
+
+        public async Task<VehicleMake> GetVehicleMakeByIdAsync(Guid id)
+        {
+            return await _context.VehicleMakes.FindAsync(id);
+        }
+
+        public async Task CreateVehicleMakeAsync(VehicleMake vehicleMake)
+        {
+            _context.VehicleMakes.Add(vehicleMake);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateVehicleMakeAsync(VehicleMake vehicleMake)
+        {
+            _context.VehicleMakes.Update(vehicleMake);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteVehicleMakeAsync(VehicleMake vehicleMake)
+        {
+            _context.VehicleMakes.Remove(vehicleMake);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<VehicleModel>> GetVehicleModelsAsync()
+        {
+            return await _context.VehicleModels.ToListAsync();
+        }
+
+        public async Task<VehicleModel> GetVehicleModelByIdAsync(Guid id)
+        {
+            return await _context.VehicleModels.FindAsync(id);
+        }
+
+        public async Task CreateVehicleModelAsync(VehicleModel vehicleModel)
+        {
+            _context.VehicleModels.Add(vehicleModel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateVehicleModelAsync(VehicleModel vehicleModel)
+        {
+            _context.VehicleModels.Update(vehicleModel);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteVehicleModelAsync(VehicleModel vehicleModel)
+        {
+            _context.VehicleModels.Remove(vehicleModel);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
